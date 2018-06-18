e2cbb105bf2b0593c8f380a3bf3dbdc3796dc1fc
Howard Tran <howard.tran@merqbiz.com>
2018-06-18 15:35:08 -0700

    updated logo.jsx to compile

--- public/path---update-password-a0e39f21c11f6a62c5ab.js
+++ public/path---update-password-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([28308881027675],{452:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([28308881027675],{475:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---update-password-a0e39f21c11f6a62c5ab.js.map
