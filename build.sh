91b88038484a65d08cb0457eb8adadf6b2ac9e5d
Howard Tran <howard.tran@merqbiz.com>
2018-06-21 16:04:51 -0700

    adding in build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+if [ "$BRANCH_ENV" = "develop" ]
+then
+    NODE_ENV="development"
+elif [ "$BRANCH_ENV" = "qa" ]
+then
+    NODE_ENV="stage"
+elif [ "$BRANCH_ENV" = "preview" ]
+then
+    NODE_ENV="preview"
+elif [ "$BRANCH_ENV" = "demo" ]
+then
+    NODE_ENV="demo"
+elif [ "$BRANCH_ENV" = "master" ]
+then
+    NODE_ENV="production"
+else
+    NODE_ENV="develop"
+fi
+echo "Building for environment" $NODE_ENV
+
+npm run build
